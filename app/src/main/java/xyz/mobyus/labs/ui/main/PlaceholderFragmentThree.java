4210b2e88c097d05e4e8b50b2e2961c24c1aad90
sjpau <theonlymoby@protonmail.com>
2022-10-01 09:51:22 +0300

    lab 3 layout

--- app/src/main/java/xyz/mobyus/labs/ui/main/PlaceholderFragmentThree.java
+++ app/src/main/java/xyz/mobyus/labs/ui/main/PlaceholderFragmentThree.java
@@ -7,7 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.mariuszgromada.math.mxparser.*;
 import xyz.mobyus.labs.R;
 
 /**
@@ -25,6 +30,33 @@ public class PlaceholderFragmentThree extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    Button btnNumber0;
+    Button btnNumber1;
+    Button btnNumber2;
+    Button btnNumber3;
+    Button btnNumber4;
+    Button btnNumber5;
+    Button btnNumber6;
+    Button btnNumber7;
+    Button btnNumber8;
+    Button btnNumber9;
+
+    TextView txtResult;
+
+    EditText edtInput;
+
+    Button btnC;
+
+    Button btnSqrR;
+    Button btnAdd;
+    Button btnSub;
+    Button btnMul;
+    Button btnDiv;
+    Button btnChangeSign;
+
+    Button btnDot;
+    Button btnResult;
 
     public PlaceholderFragmentThree() {
         // Required empty public constructor
@@ -61,6 +93,8 @@ public class PlaceholderFragmentThree extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_placeholder_three, container, false);
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_placeholder_two, container, false);
+        return view;
     }
 }
