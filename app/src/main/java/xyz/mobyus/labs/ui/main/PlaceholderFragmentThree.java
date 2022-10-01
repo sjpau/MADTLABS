d7d0441d3e42ddd7a94b5e5ff974b6ac3ab70515
sjpau <theonlymoby@protonmail.com>
2022-10-01 10:37:50 +0300

    removed more autogen garbage

--- app/src/main/java/xyz/mobyus/labs/ui/main/PlaceholderFragmentThree.java
+++ app/src/main/java/xyz/mobyus/labs/ui/main/PlaceholderFragmentThree.java
@@ -18,15 +18,6 @@ import xyz.mobyus.labs.R;
 
 public class PlaceholderFragmentThree extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     Button btnNumber0;
     Button btnNumber1;
     Button btnNumber2;
@@ -69,10 +60,6 @@ public class PlaceholderFragmentThree extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
